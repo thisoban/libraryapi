5d6917df7886ea2c61fe67dfbb95d5d3ece8d022
thisoban <thisoban_1995@hotmail.com>
2022-11-15 19:39:43 +0100

    command seriecreate

---
+++ src/Application/Series/Commands/CreateSeries/CreateSeriesCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+
+namespace CleanArchitecture.Application.Series.Commands.CreateSeries;
+public class CreateSeriesCommand
+{
+    public string? title { get; set; }
+}
+public class CreateSerieCommandHandler : CreateSeriesCommand
+{
+    private readonly IApplicationDbContext _context;
+    public CreateSerieCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public Task<int> Handle(CreateSeriesCommand request, CancellationToken cancellationToken)
+    {
+        
+        throw new NotImplementedException();
+    }
+}
